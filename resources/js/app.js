c64548e494ceb2c706b8c686f778237ada72e432
Jhonatan Morais <jhonatan.morais@gmail.com>
2020-11-18 01:29:30 -0300

    Vuetify 3 installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './views/app.vue'
 import router from "./router";
 import store from "./store";
+import Vuetify from "vuetify";
+import 'vuetify/dist/vuetify.min.css'
 
-//Main pages
+Vue.use(Vuetify)
 
 const app = new Vue({
     el: '#app',
     router,
     store,
+    vuetify: new Vuetify({}),
     components: { App },
 
 });
--- resources/js/views/Page1.vue
+++ resources/js/views/Page1.vue
@@ -1,9 +1,21 @@
 <template>
-    <div>Welcome to page 1 - Data from storage {{ $store.getters.data }}</div>
+    <div>
+        <div>
+            Welcome to page 1 - Data from storage {{ $store.getters.data }}
+        </div>
+        <div>
+            <Carousel />
+        </div>
+    </div>
 </template>
 
 <script>
-export default {};
+import Carousel from "../components/carousel";
+export default {
+    components: {
+        Carousel
+    }
+};
 </script>
 
 <style></style>
